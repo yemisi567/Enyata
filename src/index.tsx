853e07c42e416fea6a17cbf0d11d045d01c67713
yemisi567 <alegbeyemi@gmail.com>
2024-04-26 00:31:00 +0100

    fix deploy error

--- src/index.tsx
+++ src/index.tsx
@@ -4,20 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { AuthProvider } from "./context/auth/AuthContext";
-import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-     <QueryClientProvider client={queryClient}>
-    <AuthProvider>
-      <Theme>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
         <App />
-      </Theme>
-    </AuthProvider>
+      </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
